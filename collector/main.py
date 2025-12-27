7d0173ca98bad1357d3f8ccb4d51edf3b891ab30
luyukai <luyukai@lenovosha64.bbrouter>
2025-12-27 06:29:11 +0000

    add dick collector

--- collector/main.py
+++ collector/main.py
@@ -3,7 +3,7 @@ from collector.cpu.cpu_collector_linux import CpuCollector
 from collector.disk.diskio_collector_linux import DiskIOCollector
 
 register = REGISTRY
-# register.register(CpuCollector())
+register.register(CpuCollector())
 register.register(DiskIOCollector())
 
 
